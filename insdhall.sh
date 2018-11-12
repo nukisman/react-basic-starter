be7ae8434a0f225173c95b3fe01ed60a1a9d091c
Alexander Nuikin <nukisman@gmail.com>
2018-11-12 20:47:59 +0300

    jq exclude some packages via psc-package.json

--- insdhall.sh
+++ insdhall.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 NAME='local'
 TARGET=.psc-package/$NAME/.set/packages.json
+EXCLUDE='$conf[0].exclude | reduce .[] as $item ($pkgs[0]; delpaths([[$item]]))'
+TMP=/tmp/packages.json
+
 mkdir -p .psc-package/$NAME/.set \
-&& ~/bin/dhall/dhall-to-json --pretty <<< './packages.dhall' \
-| jq 'del(.chanpon)' \
-> $TARGET \
+&& ~/bin/dhall/dhall-to-json --pretty <<< './packages.dhall' > $TMP \
+&& jq -nMr --slurpfile pkgs $TMP --slurpfile conf psc-package.json '$conf[0].exclude | reduce .[] as $item ($pkgs[0]; delpaths([[$item]]))' > $TARGET \
 && echo wrote packages.json to $TARGET \
 && psc-package install
