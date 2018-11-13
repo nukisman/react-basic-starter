b3bf8f80efa1209ffee7e560f12c359d9e9b9b4f
Alexander Nuikin <nukisman@gmail.com>
2018-11-13 22:05:02 +0300

    insdhall.sh: verifyDeps

--- insdhall.sh
+++ insdhall.sh
@@ -4,10 +4,19 @@ TARGET=.psc-package/$NAME/.set/packages.json
 EXCLUDE='$conf[0].exclude | reduce .[] as $item ($pkgs[0]; delpaths([[$item]]))'
 TMP=/tmp/packages.json
 
+verifyDeps() {
+  deps=`cat psc-package.json | jq -r '.depends | .[]'`;
+  for p in $deps;
+  do
+    echo "> Verify $p";
+    psc-package verify $p;
+  done
+}
+
 mkdir -p .psc-package/$NAME/.set \
 && echo 'Generate packages.json ...' \
 && dhall-to-json --pretty <<< './packages.dhall' > $TMP \
 && jq -nMr --slurpfile pkgs $TMP --slurpfile conf psc-package.json "$EXCLUDE" > $TARGET \
 && echo wrote packages.json to $TARGET \
-&& psc-package install \
+&& verifyDeps \
 && yarn docs
