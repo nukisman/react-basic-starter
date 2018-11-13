d118a7161b2a11021cbf83a4c6735a714cdb96c9
Alexander Nuikin <nukisman@gmail.com>
2018-11-13 19:34:43 +0300

    Shorter lines in insdhall.sh

--- insdhall.sh
+++ insdhall.sh
@@ -7,7 +7,7 @@ TMP=/tmp/packages.json
 mkdir -p .psc-package/$NAME/.set \
 && echo 'Generate packages.json ...' \
 && dhall-to-json --pretty <<< './packages.dhall' > $TMP \
-&& jq -nMr --slurpfile pkgs $TMP --slurpfile conf psc-package.json '$conf[0].exclude | reduce .[] as $item ($pkgs[0]; delpaths([[$item]]))' > $TARGET \
+&& jq -nMr --slurpfile pkgs $TMP --slurpfile conf psc-package.json "$EXCLUDE" > $TARGET \
 && echo wrote packages.json to $TARGET \
 && psc-package install \
 && yarn docs
